50bfc5b66ba3a87b94d68850e6a71da57b5fa0a6
miyamoto <ryohei1000000000@gmail.com>
2021-09-20 12:50:40 +0900

    change

--- frontend/src/Ranking.tsx
+++ frontend/src/Ranking.tsx
@@ -9,7 +9,13 @@ const Ranking = (props: Props) => {
   let imageList: JSX.Element[] = [];
   if (props.images != null) {
     imageList = props.images.map((image) => (
-      <li key={image.id}>{image.name}</li>
+      <li key={image.id}>
+        <div>
+          <p>{image.name}</p>
+          <p>{image.good}</p>
+          <p>{image.nope}</p>
+        </div>
+      </li>
     ));
   }
   return <div>{<ul>{imageList}</ul>}</div>;
